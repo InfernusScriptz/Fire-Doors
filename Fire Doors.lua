db3af869a3301ab44885d58dff138569b5fd9565
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2023-05-08 17:38:03 +0300

    FireDoors.lua
    ok

---
+++ Fire Doors.lua
@@ -0,0 +1,2 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Fire Doors [2.0.0]", "Synapse")
