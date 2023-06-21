ddd766d7fed219fb026e144e974384bb7d25a54e
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2023-06-21 15:03:39 +0300

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -14,6 +14,7 @@ local List = {
 	["dzntzcuzcool"] = yes,
 	["just_Misha2012"] = yes,
 	["qwerty224537"] = yes,
+	["6uggjkki"] = yes,
 	["AydAwesome"] = yes,
 	["Justintwintwo"] = yes,
 	["DieTime4You"] = yes,
