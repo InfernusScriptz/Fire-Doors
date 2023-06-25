5b37cec73f7b68851f4e3f722b8f13c0fdcb8385
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2023-06-25 13:53:19 +0300

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,7 +1,7 @@
 local yes = "whitelisted"
 local no = "blacklisted"
 local List = {
-	
+	["57a436ab07d5fa7cc1047b8ac141857919afa65845d9ba57dd59cb34ead961a67dbd5cfeef99fd4c9abe758c3b5fdbdb"] = yes,
 }
 local list = {}
 for i,v in pairs(List) do
