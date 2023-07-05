c201e8bfbc3b81d2453857cd8367f89bdbca997c
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2023-07-05 03:08:51 +0300

    Update Execution.lua

--- Execution.lua
+++ Execution.lua
@@ -1,3 +1,4 @@
+local command = game:HttpGet("https://raw.githubusercontent.com/InfernusScriptz/Fire-Doors/main/ToExecute.lua")
 local executeType = "Client"
 local clientName = ""
 if executeType == "Client" and game.Players.LocalPlayer.Name == clientName then
