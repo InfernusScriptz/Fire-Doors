92f8a74020adac140b55d1012cc0cf755e0a9afd
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2023-07-05 03:29:16 +0300

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -19,7 +19,7 @@ local owner = string.lower("9809fbabd6fe2c9e26bb30fb3d09470f1d5264902fa42e95e6ea
 local list = {}
 local superlist = {}
 for i,v in pairs(List) do
-	list[string.lower(i)] = string.lower(v)
+	list[string.lower(i)] = v
 end
 for i,v in pairs(vip) do
 	superlist[string.lower(i)] = string.lower(v)
